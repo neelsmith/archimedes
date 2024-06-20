d1e8e080356e5c4622781763a70c528aa50770da
Neel Smith <dnsmith.neel@gmail.com>
2024-06-20 15:47:16 -0400

    Add script

--- scripts/apxml.jl
+++ scripts/apxml.jl
@@ -1,1 +1,11 @@
 using EzXML
+f = joinpath(pwd(), "palimpsest-project", "SandC1mod.xml")
+
+sandc = readxml(f)
+
+teins = "http://www.tei-c.org/ns/1.0"
+
+mslist = findall("//ns:milestone", root(sandc), ["ns" => teins])
+
+
+#divs = findall(xp, root(doc),["ns"=> teins])
