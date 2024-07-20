67bcd6a2f461c2da8055e74d16d86c3e364a01d1
Neel Smith <dnsmith.neel@gmail.com>
2024-07-20 08:39:49 -0400

    Reparse

--- scripts/parselist.jl
+++ scripts/parselist.jl
@@ -1,41 +1,58 @@
-# "euclid-" or ""
-prefix = "euclid-" 
-# prefix = ""
-
-#using Downloads
 using CitableParserBuilder, CitableBase
 using Kanones
 
-parserurl = "http://shot.holycross.edu/morphology/attic_core-current.cex"
-parser = kParser(parserurl, UrlReader)
+datadir = joinpath(pwd(), "data")
 
-countsfile = joinpath(pwd(), "data", "$(prefix)tokensnormed.cex")
+parserurl = "http://shot.holycross.edu/morphology/math-current.cex"
+p = kParser(parserurl, UrlReader)
+
+prefixes = Dict(
+	:euclid => "euclid-",
+	:archimedes => ""
+)
 
 
-lns = readlines(countsfile)[2:end]
-tokens = map(lns) do ln
-	split(ln, "|")[1]
+function parselist(; parser = p,  text = :archimedes, prefixdict = prefixes, datadir = datadir)
+	prefix = prefixdict[text]
+	countsfile = joinpath(datadir, "$(prefix)tokensnormed.cex")
+	lns = readlines(countsfile)[2:end]
+	tokens = map(lns) do ln
+		split(ln, "|")[1]
+	end
+	parses = map(tokens) do t
+		parsetoken(t, parser)
+	end
+	parsecounts = map(v -> length(v), parses)
+
+	summarylines =  ["token|count|parses"]
+	for (i,p) in enumerate(lns)
+		msg = parsecounts[i] > 0 ? p * "|yes" : p * "|no"
+		push!(summarylines, msg)
+	end
+
+	parseress = joinpath(datadir, "$(prefix)parseresults.cex")
+	open(parseress,"w") do io
+		write(io, join(summarylines, "\n"))
+	end
+
+	failures = filter(ln -> endswith(ln,"|no"), summarylines)
+	failsfile = joinpath(datadir, "$(prefix)failures.cex")
+	open(failsfile,"w") do io
+		write(io, join(failures,"\n"))
+	end
 end
 
-parses = map(tokens) do t
-	parsetoken(t, parser)
-end
-parsecounts = map(v -> length(v), parses)
+
+parselist()
+parselist(text = :euclid)
 
 
-summarylines =  ["token|count|parses"]
-for (i,p) in enumerate(lns)
-	msg = parsecounts[i] > 0 ? p * "|yes" : p * "|no"
-	push!(summarylines, msg)
-end
 
-parseress = joinpath(pwd(), "data", "$(prefix)parseresults.cex")
-open(parseress,"w") do io
-	write(io, join(summarylines, "\n"))
-end
 
-failures = filter(ln -> endswith(ln,"|no"), summarylines)
-failsfile = joinpath(pwd(), "data", "$(prefix)failures.cex")
-open(failsfile,"w") do io
-	write(io, join(failures,"\n"))
-end
+
+
+
+
+
+
+
