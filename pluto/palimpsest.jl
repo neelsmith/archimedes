71fca1bcb189d4716f95d3e149d1b0daeac6735e
Neel Smith <dnsmith.neel@gmail.com>
2024-07-29 16:47:45 -0400

    Add files

---
+++ pluto/palimpsest.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.19.42
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 69a91088-4dc5-11ef-3134-fbafe6c5f787
+begin
+	
+end
+
+# ╔═╡ cfe7086e-4b62-438a-88cc-b4c27e3ce7d7
+md"""# Palimpsest"""
+
+# ╔═╡ Cell order:
+# ╠═69a91088-4dc5-11ef-3134-fbafe6c5f787
+# ╟─cfe7086e-4b62-438a-88cc-b4c27e3ce7d7
